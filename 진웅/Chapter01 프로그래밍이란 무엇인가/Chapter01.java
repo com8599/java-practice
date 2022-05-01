bceae84337d247a1bdc5703cbe9e1819e91f5eec
Jeong Jin Ung <jinung.dev@gmail.com>
2022-05-01 23:15:18 +0900

    chapter01 정리

---
+++ 진웅/Chapter01 프로그래밍이란 무엇인가/Chapter01.java
@@ -0,0 +1,22 @@
+
+// 1. Calculator라는 이름을 같는 클래스 만들어 보기
+public class Calculatror {
+
+}
+
+// 2. 빼기, 곱하기, 나누기를 하고 그 결과를 리턴해주는 메소드를 만들어 보자
+public class Calculator {
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+}
