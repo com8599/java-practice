24eb96c1cdc1e650840a32c659704f893503f480
com8599 <com8599@gmail.com>
2022-05-02 16:37:43 +0900

    feat: pass by value, pass by reference

---
+++ 답이없다잉/Chapter08/ReferencePass.java
@@ -0,0 +1,44 @@
+package Chapter08;
+
+public class ReferencePass {
+    public static void main(String[] args) {
+        ReferencePass referencePass = new ReferencePass();
+        referencePass.callPassByValue();
+        referencePass.callPassByReference();
+    }
+
+    private void callPassByValue() {
+        int a = 10;
+        String b = "b";
+        System.out.println("before passByValue");
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        passByValue(a, b);
+        System.out.println("after passByValue");
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+
+    private void passByValue(int a, String b) {
+        a = 20;
+        b = "z";
+        System.out.println("in passByValue");
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+
+    private void callPassByReference() {
+        MemberDTO memberDTO = new MemberDTO("TulipLee");
+        System.out.println("before passByReference");
+        System.out.println("member.name=" + memberDTO.name);
+        passByReference(memberDTO);
+        System.out.println("after passByReference");
+        System.out.println("member.name=" + memberDTO.name);
+    }
+
+    private void passByReference(MemberDTO memberDTO) {
+        memberDTO.name = "TwoLee";
+        System.out.println("in passByReference");
+        System.out.println("member.name=" + memberDTO.name);
+    }
+}
