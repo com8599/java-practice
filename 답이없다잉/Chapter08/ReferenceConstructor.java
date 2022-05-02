19acb597138fbb2e1a0722846f94aadc33e34973
com8599 <com8599@gmail.com>
2022-05-02 15:51:53 +0900

    feat: 193

---
+++ 답이없다잉/Chapter08/ReferenceConstructor.java
@@ -0,0 +1,15 @@
+package 답이없다잉.Chapter08;
+
+public class ReferenceConstructor {
+    public static void main(String[] args) {
+        ReferenceConstructor referenceConstructor = new ReferenceConstructor();
+        referenceConstructor.makeMemberObject();
+    }
+
+    private void makeMemberObject() {
+        MemberDTO dto1 = new MemberDTO();
+        MemberDTO dto2 = new MemberDTO("TulipLee");
+        MemberDTO dto3 = new MemberDTO("TulipLee", "010xxxxxxxx");
+        MemberDTO dto4 = new MemberDTO("TulipLee", "010xxxxxxxx", "com8599@gmail.com");
+    }
+}
