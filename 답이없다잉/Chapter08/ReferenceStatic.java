2265c4dfa9ebf185cb584cecde01d7dbe697f391
com8599 <com8599@gmail.com>
2022-05-02 16:20:53 +0900

    feat: Static 메소드와 일반 메소드의 차이

---
+++ 답이없다잉/Chapter08/ReferenceStatic.java
@@ -0,0 +1,17 @@
+package Chapter08;
+
+public class ReferenceStatic {
+    public static String name = "Min";
+
+    public static void main(String[] args) {
+        ReferenceStatic.staticMethod();
+    }
+
+    private static void staticMethod() {
+        System.out.println("This is a staticMethod!");
+    }
+
+    public static void staticMethodCallVariable() {
+        System.out.println(name);
+    }
+}
---
+++ 답이없다잉/Chapter08/ReferenceStaticVariable.java
@@ -0,0 +1,24 @@
+package Chapter08;
+
+public class ReferenceStaticVariable {
+    String name;
+
+    public ReferenceStaticVariable() {
+    }
+
+    public ReferenceStaticVariable(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        ReferenceStaticVariable referenceStaticVariable = new ReferenceStaticVariable();
+        referenceStaticVariable.checkName();
+    }
+
+    private void checkName() {
+        ReferenceStaticVariable referenceStaticVariable = new ReferenceStaticVariable("TulipLee");
+        System.out.println(referenceStaticVariable.name);
+        ReferenceStaticVariable referenceStaticVariable2 = new ReferenceStaticVariable("TwoLee");
+        System.out.println(referenceStaticVariable.name);
+    }
+}
