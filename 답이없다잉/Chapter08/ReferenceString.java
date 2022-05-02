4f58ad296a528511692b1f76414b5e60fabb7bb7
com8599 <com8599@gmail.com>
2022-05-02 15:33:34 +0900

    feat: c8-2

--- 답이없다잉/Chapter08/ReferenceString.java
+++ 답이없다잉/Chapter08/ReferenceString.java
@@ -1,6 +1,8 @@
 package 답이없다잉.Chapter08;
 
 public class ReferenceString {
+    String instanceVariable;
+
     public ReferenceString(String str) {
     }
 
@@ -9,5 +11,13 @@ public class ReferenceString {
 
     public static void main(String[] args) {
         ReferenceString referenceString = new ReferenceString();
+    }
+
+    public String getInstanceVariable() {
+        return instanceVariable;
+    }
+
+    public void setInstanceVariable(String str) {
+        instanceVariable = str;
     }
 }
