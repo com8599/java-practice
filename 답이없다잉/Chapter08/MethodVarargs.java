dc46b5d0bd538974fd1744be80a5eeb44e04cba6
com8599 <com8599@gmail.com>
2022-05-02 16:42:35 +0900

    feat: 매개변수를 지정하는 특이한 방법

---
+++ 답이없다잉/Chapter08/MethodVarargs.java
@@ -0,0 +1,20 @@
+package Chapter08;
+
+public class MethodVarargs {
+    public static void main(String[] args) {
+        MethodVarargs varargs = new MethodVarargs();
+        varargs.calculateNumbersWithArray(new int[]{1,2,3,4,5});
+        varargs.calculateNumbers(1,2,3,4,5);
+    }
+
+    private void calculateNumbersWithArray(int[] numbers) {
+    }
+
+    private void calculateNumbers(int...numbers) {
+        int total = 0;
+        for (int number : numbers) {
+            total += number;
+        }
+        System.out.println("Total=" + total);
+    }
+}
