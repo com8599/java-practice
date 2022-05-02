ed96ab7013eabc4adcbc0b30308d3e076f841dcb
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-05-02 16:11:24 +0900

    Create 자바의 신 8강 문제.java

---
+++ 김동균/chapter08/자바의 신 8강 문제.java
@@ -0,0 +1,52 @@
+class Student {
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, String address, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
+class ManageStudent {
+    public static void main(String[] args) {
+        ManageStudent manageStudent = new ManageStudent();
+        Student[] students;
+
+        students = manageStudent.addStudent();
+        manageStudent.printStudents(students);
+    }
+
+    public Student[] addStudent() {
+        Student[] students = new Student[3];
+        students[0] = new Student("Lim");
+        students[1] = new Student("Min");
+        students[2] = new Student("Sook", "Seoul", "010xxxxxxxx", "example@naver.com");
+
+        return students;
+    }
+
+    public void printStudents (Student... students) {
+        for(Student student : students) {
+            System.out.println(student.toString());
+        }
+    }
+}
