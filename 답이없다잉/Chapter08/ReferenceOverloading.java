4e0ef69bf58cea4660ee20224feffac390310095
com8599 <com8599@gmail.com>
2022-05-02 15:54:23 +0900

    feat: 197

---
+++ 답이없다잉/Chapter08/ReferenceOverloading.java
@@ -0,0 +1,23 @@
+package 답이없다잉.Chapter08;
+
+public class ReferenceOverloading {
+    public static void main(String[] args) {
+        ReferenceOverloading referenceOverloading = new ReferenceOverloading();
+    }
+
+    public void print(int data) {
+
+    }
+
+    public void print(String data) {
+
+    }
+
+    public void print(int intData, String strData) {
+
+    }
+
+    public void print(String strData, int intData) {
+
+    }
+}
