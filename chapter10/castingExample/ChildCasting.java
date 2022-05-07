cbee56f1160b3138692a44fd6a4eac4e981354e3
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-05-07 15:01:41 +0900

    Create ChildCasting.java

---
+++ chapter10/castingExample/ChildCasting.java
@@ -0,0 +1,20 @@
+package com.example.godofjava.chapter10.castingExample;
+
+public class ChildCasting extends ParentCasting {
+
+    public ChildCasting() {
+
+    }
+
+    public ChildCasting(String name) {
+
+    }
+
+    public void printName() {
+        System.out.println("printName() - Child");
+    }
+
+    public void printAge() {
+        System.out.println("printAge() - 18 month");
+    }
+}
