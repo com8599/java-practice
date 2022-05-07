e18808ed6e1f022ce31a1b87557ce33f02f2a926
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-05-07 15:02:46 +0900

    Create Dog.java

---
+++ chapter10/solution/Dog.java
@@ -0,0 +1,9 @@
+package com.example.godofjava.chapter10.solution;
+
+public class Dog extends Animal {
+
+    @Override
+    public void move() {
+        System.out.println("잔디밭을 뛰어 다닙니다.");
+    }
+}
