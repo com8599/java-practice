8f11f2a8ee99acebc82d9deae2b42f61e0d21db1
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-05-07 15:01:57 +0900

    Create ParentCasting.java

---
+++ chapter10/castingExample/ParentCasting.java
@@ -0,0 +1,16 @@
+package com.example.godofjava.chapter10.castingExample;
+
+public class ParentCasting {
+
+    public ParentCasting() {
+
+    }
+
+    public ParentCasting(String name) {
+
+    }
+
+    public void printName() {
+        System.out.println("printName() - Parent");
+    }
+}
