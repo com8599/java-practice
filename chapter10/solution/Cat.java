e151be2b4d6b69a3803f9b3f18f22b6670b67865
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-05-07 15:02:34 +0900

    Create Cat.java

---
+++ chapter10/solution/Cat.java
@@ -0,0 +1,9 @@
+package com.example.godofjava.chapter10.solution;
+
+public class Cat extends Animal {
+
+    @Override
+    public void move() {
+        System.out.println("캣타워로 깡총 뛰어올라갑니다.");
+    }
+}
