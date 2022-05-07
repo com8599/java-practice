bd9c686d5d4f2e532d7e7e7a251baa21f2132353
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-05-07 15:02:19 +0900

    Create Animal.java

---
+++ chapter10/solution/Animal.java
@@ -0,0 +1,17 @@
+package com.example.godofjava.chapter10.solution;
+
+public class Animal {
+    String name;
+    String kind;
+    int legCount;
+    int iq;
+    boolean hasWing;
+
+    public void move() {
+
+    }
+
+    public void eatFood(String name) {
+        System.out.println(name + "을 먹습니다.");
+    }
+}
